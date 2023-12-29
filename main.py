1ef9580da79063e539cb684e9bdd38076d5afc44
Krishan Walia <krishanw30@gmail.com>
2023-12-29 07:19:22 +0530

    Data Ingestion Completed

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from titanic.logging import logger
+from titanic.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Logging working fine")
+
+STAGE_NAME = 'Data Ingestion Stage'
+
+try:
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<< \n\n x=========================x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
