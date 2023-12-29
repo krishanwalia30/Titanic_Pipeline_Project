aa1d5fdf9a6b6a7668aa969dd1ba91f6faba188f
Krishan Walia <krishanw30@gmail.com>
2023-12-29 12:39:14 +0530

    Data Validation completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from titanic.logging import logger
 from titanic.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from titanic.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = 'Data Ingestion Stage'
@@ -8,6 +9,19 @@ try:
     logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<< \n\n x=========================x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = 'Data Validation Stage'
+
+try:
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<< \n\n x=========================x")
 
 except Exception as e:
--- src/titanic/config/configuration.py
+++ src/titanic/config/configuration.py
@@ -1,5 +1,5 @@
 from titanic.constants import *
-from titanic.entity import DataIngestionConfig
+from titanic.entity import DataIngestionConfig, DataValidationConfig
 from titanic.utils.common import read_yaml, create_directories
 
 class ConfigurationManager:
@@ -27,3 +27,16 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir= config.root_dir,
+            STATUS_FILE= config.STATUS_FILE,
+            ALL_REQUIRED_FILES= config.ALL_REQUIRED_FILES
+        )
+
+        return data_validation_config
