b7302663904de2befe265ccd6d212073a2069ccd
Krishan Walia <krishanw30@gmail.com>
2023-12-31 14:26:28 +0530

    DVC initiated

--- src/titanic/pipeline/stage_01_data_ingestion.py
+++ src/titanic/pipeline/stage_01_data_ingestion.py
@@ -12,3 +12,16 @@ class DataIngestionTrainingPipeline:
         data_ingestion = DataIngestion(config=data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
+
+if __name__ == __name__:
+    try:
+        STAGE_NAME = 'Data Ingestion Stage'
+
+        logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<")
+        data_ingestion = DataIngestionTrainingPipeline()
+        data_ingestion.main()
+        logger.info(f">>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<< \n\n x=========================x \n\n")
+
+    except Exception as e:
+        logger.exception(e)
+        raise e
