4ea4db746bf6d47525aef4a185e2ee2ffdae100d
Krishan Walia <krishanw30@gmail.com>
2024-01-03 19:29:55 +0530

    Hyper Parameters altered

--- src/titanic/utils/common.py
+++ src/titanic/utils/common.py
@@ -105,23 +105,24 @@ def evaluate_models(x_train, y_train,x_test, y_test, models, param):
             model = list(models.values())[i]
             para = param[list(models.keys())[i]]
 
-            gs = GridSearchCV(model, para, cv=3)
+            gs = GridSearchCV(model, para, cv=2)
             gs.fit(x_train, y_train)
 
             model.set_params(**gs.best_params_)
+            print(f"================================{gs.best_params_}================================")
             model.fit(x_train, y_train) # Training the Model
 
-            y_train_pred = model.predict(x_train)
+            # y_train_pred = model.predict(x_train)
 
             y_test_pred = model.predict(x_test)
 
-            train_model_score = r2_score(y_train,y_train_pred)
+            # train_model_score = r2_score(y_train,y_train_pred)
 
             test_model_score = r2_score(y_test,y_test_pred)
 
             report[list(models.keys())[i]]= test_model_score
 
-            return report
+        return report
 
     except Exception as e:
         logger.exception(e)
